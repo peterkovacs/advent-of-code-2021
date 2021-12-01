d9fd762b9ba1d59fc3e8f9e2227e5789cfb5e075
Peter Kovacs <peter@sanebox.com>
2021-12-01 08:10:56 -0500

    Day 1

--- Sources/AdventOfCode/Day1.swift
+++ Sources/AdventOfCode/Day1.swift
@@ -4,6 +4,18 @@ import ArgumentParser
 import Algorithms
 
 struct Day1: ParsableCommand {
+    static let input = stdin.compactMap(Int.init)
     func run() {
+
+        let part1 = zip(Self.input, Self.input.dropFirst()).reduce(0) { $0 + (($1.0 < $1.1) ? 1 : 0)}
+        print("Part 1: \(part1)")
+
+        let part2 =
+        zip(
+            Self.input.indices.dropLast(2).map{ Self.input[$0..<$0+3].reduce(0,+) },
+            Self.input.indices.dropLast(2).dropFirst().map{ Self.input[$0..<$0+3].reduce(0,+) }
+        ).reduce(0) { $0 + (($1.0 < $1.1) ? 1 : 0)}
+
+        print("Part 2: \(part2)")
     }
 }
