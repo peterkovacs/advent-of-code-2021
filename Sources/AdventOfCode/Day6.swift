54d815f75591d6c21e9a837058986a89dfaf4a3d
Peter Kovacs <peter@sanebox.com>
2021-12-06 08:16:24 -0500

    Day 6

--- Sources/AdventOfCode/Day6.swift
+++ Sources/AdventOfCode/Day6.swift
@@ -2,8 +2,56 @@
 import Foundation 
 import ArgumentParser
 import Algorithms
+import Parsing
 
 struct Day6: ParsableCommand {
+    static let parser = Many(
+        Int.parser()
+            .skip(Optional.parser(of: ","))
+    )
+    
+    static let input = parser.parse(stdin.joined(separator: ""))!
+    
+    func part1() {
+        var ages = Dictionary(lanternFish: Self.input)
+        for _ in 0..<80 {
+            ages = ages.nextDay
+        }
+        
+        print("Part 1", ages.values.reduce(0, +))
+    }
+    
+    func part2() {
+        var ages = Dictionary(lanternFish: Self.input)
+        for _ in 0..<256 {
+            ages = ages.nextDay
+        }
+        
+        print("Part 2", ages.values.reduce(0, +))
+    }
+
     func run() {
+        part1()
+        part2()
+    }
+}
+
+extension Dictionary where Key == Int, Value == Int {
+    init(lanternFish input: [Int]) {
+        self = input.reduce(into: [:]) {
+            $0[$1, default: 0] += 1
+        }
+    }
+    
+    var nextDay: Self {
+        reduce(into: Self()) {
+            switch $1.key {
+            case 0:
+                $0[8, default: 0] += $1.value
+                $0[6, default: 0] += $1.value
+            default:
+                $0[$1.key - 1, default: 0] += $1.value
+            }
+        }
     }
 }
